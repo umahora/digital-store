9744436ed306cbc0b99db6bc22c7a1ba6d820a6f
umahora <20365605+umahora@users.noreply.github.com>
2025-05-24 21:17:46 -0300

    add export assets index.js

---
+++ src/assets/index.js
@@ -0,0 +1,38 @@
+import Tennis01 from "./tennis/tennis01.png"
+import Tennis02 from "./tennis/tennis02.png"
+import Tennis03 from "./tennis/tennis03.png"
+import Tennis04 from "./tennis/tennis04.png"
+import Tennis05 from "./tennis/tennis05.png"
+import Tennis06 from "./tennis/tennis06.png"
+import Tennis07 from "./tennis/tennis07.png"
+
+
+
+import ArrowLeft from "./icons/arrow-left.svg"
+import ArrowRight from "./icons/arrow-right.svg"
+import Facebook from "./icons/facebook.svg"
+import Instagram from "./icons/instagram.svg"
+import LogoFooter from "./icons/logo-footer.svg"
+import LogoHeader from "./icons/logo-header.svg"
+import Lupa from "./icons/lupa.svg"
+import MenuBtn from "./icons/menu-btn.svg"
+import MiniCart from "./icons/mini-cart.svg"
+import react from "./icons/react.svg"
+import StarIcon from "./icons/star-icon.svg"
+import Twitter from "./icons/twitter.svg"
+
+
+export {
+    ArrowLeft,
+    ArrowRight,
+    Facebook,
+    Instagram,
+    LogoFooter,
+    LogoHeader,
+    Lupa,
+    MenuBtn,
+    MiniCart,
+    react,
+    StarIcon,
+    Twitter
+}
