723f7cb5f87e0735f5067553abeaab42f88b9164
umahora <20365605+umahora@users.noreply.github.com>
2025-05-26 22:16:18 -0300

    add vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
     <>
         <App/>
+        <Analytics/>
     </>,
 )
