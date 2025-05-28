ebe4fd82045f5155a4b2f1b4f63f77af788dc3df
umahora <20365605+umahora@users.noreply.github.com>
2025-05-28 19:36:57 -0300

    add vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vite.dev/config/
@@ -8,6 +9,14 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    svgr({
+      svgrOptions: {
+        replaceAttrValues: {
+          '#000': 'currentColor',
+          '#000000': 'currentColor', // In my project, all icons are black by default so I just stick to replacing black colors
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
